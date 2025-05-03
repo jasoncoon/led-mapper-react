6c8a4806c1ee4b749d6f6bb4e6eb87a268aecc26
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2025-05-03 13:26:43 -0500

    Rework Preview, add controls.

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,12 +1,17 @@
+import { Checkbox, Radio, Space } from "antd";
+import { useState } from "react";
 import { LedMap } from "../input/layout";
 import Canvas from "./Canvas";
 import { DrawFunction } from "./CanvasHook";
 
-export default function Preview({ ledMap, onFpsChange }: { ledMap?: LedMap, onFpsChange?: (fps: number) => void }) {
+export default function Preview({ ledMap }: { ledMap?: LedMap }) {
+  const [drawType, setDrawType] = useState<'leds' | 'numbers'>('numbers');
+  const [drawFps, setDrawFps] = useState<boolean>(true);
+
   let elapsed = 0;
   let fps = 0;
   let offset = 0;
-  const offsetIncrement = 1.0;
+  const offsetIncrement = 0.001;
 
   const onDraw: DrawFunction<CanvasRenderingContext2D> = (context, _frameCount, elapsedTime) => {
     elapsed += elapsedTime;
@@ -14,26 +19,23 @@ export default function Preview({ ledMap, onFpsChange }: { ledMap?: LedMap, onFp
     if (elapsed >= 1_000) {
       fps = 1_000 / elapsedTime;
       elapsed = 0;
-      onFpsChange?.(fps);
     }
 
     if (!ledMap) return;
 
     const { leds, minX, minY, height, width } = ledMap;
-    if (!leds.length) return;
+    const ledCount = leds.length;
+    if (!ledCount) return;
 
     offset += offsetIncrement;
-    if (offset > 255) offset = 0;
+    if (offset > 1) offset = 0;
 
     const canvasHeight = context.canvas.height;
     const canvasWidth = context.canvas.width;
 
-    const ledWidth = 10;
-    const ledHeight = 10;
+    const ledDiameter = Math.max(canvasWidth, canvasHeight) / Math.max(width, height);
 
-    const center = ledWidth / 2;
-
-    console.log({ canvasHeight, canvasWidth, width, height, ledWidth, ledHeight });
+    const ledCenter = ledDiameter / 2;
 
     context.globalCompositeOperation = "source-over";
     context.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -41,28 +43,65 @@ export default function Preview({ ledMap, onFpsChange }: { ledMap?: LedMap, onFp
     context.fillRect(0, 0, canvasWidth, canvasHeight);
     context.globalCompositeOperation = "lighter";
 
-    for (const led of leds) {
-      const x = (led.x - minX) * ledWidth;
-      const y = (led.y - minY) * ledHeight;
+    context.font = `${(ledDiameter / 4).toString()}px monospace`;
 
-      context.fillStyle = 'red';
-      context.beginPath();
-      context.ellipse(x + center, y + center, center, center, 0, 0, Math.PI * 2, false);
-      context.fill();
+    for (const led of leds) {
+      const hue = ((led.index / ledCount) + offset) % 1;
+
+      const fillStyle = `hsl(${(hue * 360).toString()}, 100%, 50%)`
+
+      const x = (led.x - minX) * ledDiameter;
+      const y = (led.y - minY) * ledDiameter;
+
+      context.fillStyle = fillStyle;
+
+      if (drawType === 'leds') {
+        context.beginPath();
+        context.ellipse(x + ledCenter, y + ledCenter, ledCenter, ledCenter, 0, 0, Math.PI * 2, false);
+        context.fill();
+      }
+
+      if (drawType === 'numbers') {
+        context.fillText(led.index.toFixed(0), x + ledCenter, y + ledCenter);
+      }
     }
 
-    // // draw FPS 
-    // context.lineWidth = 1;
-    // context.textBaseline = "top";
-    // context.textAlign = "left";
-    // context.font = `12px monospace`;
-    // context.fillStyle = 'white';
-    // context.fillText(`${fps.toLocaleString(undefined, { maximumFractionDigits: 1 })} FPS`, 10, 10);
+    // draw FPS 
+    if (drawFps) {
+      context.lineWidth = 1;
+      context.textBaseline = "top";
+      context.textAlign = "left";
+      context.font = `12px monospace`;
+      context.fillStyle = 'white';
+      context.fillText(`${fps.toLocaleString(undefined, { maximumFractionDigits: 1 })} FPS`, 10, 10);
+    }
+
+    // console.log({ canvasHeight, canvasWidth, width, height, ledDiameter, ledCenter });
   };
 
-  return (<Canvas<CanvasRenderingContext2D>
-    contextType={'2d'}
-    draw={onDraw}
-    style={{ height: 800, width: 800 }} // TODO: auto resize canvas
-  />);
+  return (
+    <>
+      <Space>
+        <Radio.Group
+          block
+          options={[
+            { label: 'LEDs', value: 'leds' },
+            { label: 'Numbers', value: 'numbers' },
+          ]}
+          onChange={(e) => {
+            setDrawType(e.target.value as 'leds' | 'numbers');
+          }}
+          optionType="button"
+          size="small"
+          value={drawType}
+        />
+        <Checkbox checked={drawFps} onChange={(e) => { setDrawFps(e.target.checked); }}>Show FPS</Checkbox>
+      </Space>
+      <Canvas<CanvasRenderingContext2D>
+        contextType={'2d'}
+        draw={onDraw}
+        style={{ height: 'calc(100% - 32px)', width: '100%' }} // TODO: auto resize canvas
+      />
+    </>
+  );
 }
